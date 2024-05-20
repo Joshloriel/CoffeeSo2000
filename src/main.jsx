8f254ccf09246fcc1d81ddd7a594a54bc7657065
josh loriel so <119284535+Joshloriel@users.noreply.github.com>
2024-05-20 11:50:04 +0800

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom'; // Correct import statement for ReactDOM
+import App from './App.jsx';
+import './index.css';
 
 // Import Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,5 +11,5 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
