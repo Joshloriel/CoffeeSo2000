5229ac49cbceb66b0ec91e38e4311cc352cd7787
josh loriel so <119284535+Joshloriel@users.noreply.github.com>
2024-05-20 11:42:48 +0800

    fixed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ['react', 'react-dom', 'bootstrap/dist/js/bootstrap.bundle.min.js'],
